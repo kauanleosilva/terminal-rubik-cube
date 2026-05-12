ea72c49298eb8b7d2f7f5b6a71d438ea40735e64
Knlss <138739462+Knlss@users.noreply.github.com>
2026-05-11 23:00:07 -0300

    feat(render): add showCube and rotateCube functions with gameplay instructions

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define MAROON "\x1b[48;5;88m"
-#define BLUE "\x1b[44m"
-#define GREEN "\x1b[42m"
-#define ROSE "\x1b[48;5;201m"
-#define YELLOW "\x1b[43m"
-#define PURPLE "\x1b[48;5;129m"
+#define MAROON "\x1b[38;5;88m"
+#define BLUE "\x1b[34m"
+#define GREEN "\x1b[32m"
+#define ROSE "\x1b[38;5;201m"
+#define YELLOW "\x1b[33m"
+#define PURPLE "\x1b[38;5;129m"
 #define RESET "\x1b[0m"
-#define BLOCK "\u2588\u2588"
+#define BLOCK "\u25A0"
 
 #ifdef _WIN32
     #include <windows.h>
@@ -16,17 +16,20 @@
 
 typedef struct Cube
 {
-    char face[6][9];
+    char faceValue[6][10];
+	int facePosition[6];
     unsigned char solution;
 } Cube;
 
 void initCube(Cube *);
+void showCube(Cube, int);
+void rotateCube(Cube *, int, int, int);
 char getColor(int);
+char * convertColor(char);
 void getAction(int *);
-void rotateCube(int, int, int);
 void getAnswer(char *);
 int classifyAnswer(char);
-void convertAnswer(char *, int *, int *, int *);
+void convertAnswer(char *, int *, int *, int *, int *);
 
 int main()
 {
@@ -39,16 +42,16 @@ int main()
         }
         SetConsoleOutputCP(65001);
     #endif
-    int userAction[3] = {-1, -1, -1};
+    int userAction[4] = {-1, -1, -1, -1};
     Cube cube;
     cube.solution = 0b00000000;
 
     initCube(&cube);
     while (1)
     {
-        // getAction(userAction);
+        getAction(userAction);
         // rotateCube(*(userAction), *(userAction + 1), *(userAction + 2));
-        printf("%s", cube.face[5]);
+        showCube(cube, 5);
         break;
     };
 
@@ -57,12 +60,27 @@ int main()
 
 void initCube(Cube *cube) {
     int f,b;
-    char color;
     for(f = 0; f < 6; f++) {
         for(b = 0; b < 9; b++) {
-            cube->face[f][b] = getColor(f);
+            cube->faceValue[f][b] = getColor(f);
         }
     };
+	for(f = 0; f < 6; f++) {
+		cube->facePosition[f] = f;
+		cube->faceValue[f][9] = '\0';
+	}
+}
+
+void showCube(Cube cube, int face) {
+	int b, count=0;
+	for(b = 0; b < 9; b++) {
+		printf(" %s%s%s", convertColor(cube.faceValue[face][b]), BLOCK, RESET);
+		count++;
+		if(count%3==0)
+		{
+			printf("\n");
+		}
+	}
 }
 
 char getColor(int option) {
@@ -71,18 +89,51 @@ char getColor(int option) {
     {
     case 0:
         color = 'M';
+        break;
     case 1:
         color = 'B';
+        break;
     case 2:
         color = 'G';
+        break;
     case 3:
         color = 'R';
+        break;
     case 4:
         color = 'Y';
+        break;
     case 5:
         color = 'P';
+        break;
     }
     return color;
+}
+
+char * convertColor(char code) {
+	char *color;
+	switch(code) {
+	case 'M':
+		color = MAROON;
+		break;
+	case 'B':
+		color = BLUE;
+		break;
+	case 'G':
+		color = GREEN;
+		break;
+	case 'R':
+		color = ROSE;
+		break;
+	case 'Y':
+		color = YELLOW;
+		break;
+	case 'P':
+		color = PURPLE;
+		break;
+	default:
+		break;
+	}
+	return color;
 }
 
 void getAction(int *userAction)
@@ -93,8 +144,8 @@ void getAction(int *userAction)
     {
         userAnswer[0] = '0';
         getAnswer(userAnswer);
-        convertAnswer(userAnswer, userAction, userAction + 1, userAction + 2);
-        printf("\n 1. %d %d %d \n", *userAction, *(userAction + 1), *(userAction + 2));
+        convertAnswer(userAnswer, userAction, userAction + 1, userAction + 2, userAction + 3);
+        printf("%d %d %d", *userAction, *(userAction + 1), *(userAction + 2));
         if (userAnswer[0] != '0')
         {
             printf("\nThe action was written incorrectly. Please try again.\n\n\n");
@@ -112,10 +163,12 @@ void getAnswer(char *userAnswer)
     printf("%22c Keys:\n\n", ' ');
     printf("First Line  %8c%10c First Column  %8c\n", 'Q', ' ', 'I');
     printf("Second Line %8c%10c Second Column %8c\n", 'W', ' ', 'O');
-    printf("Third Line  %8c%10c Third Column  %8c\n", 'E', ' ', 'E');
+    printf("Third Line  %8c%10c Third Column  %8c\n", 'E', ' ', 'P');
     printf("Up          %8c%10c Right         %8c\n", 'U', ' ', 'R');
     printf("Down        %8c%10c Left          %8c\n\n", 'D', ' ', 'L');
-    printf("Enter your move (You can type in any order. Do not use spaces)\n");
+    printf("Tip 1: To rotate the face, type LCM in any order.\n");
+    printf("Tip 2: To rotate the cube, press only the corresponding movement key, preceded by the \"F\" key.\n");
+    printf("Tip 3: To rotate the front face, press only the corresponding movement key (\"R\" for CW or \"L\" for CCW), preceded by the \"S\" key.\n\n");
     printf("Your move: ");
     scanf("%3s", userAnswer + 1);
 }
@@ -149,7 +202,7 @@ int classifyAnswer(char userAnswer)
     }
 }
 
-void convertAnswer(char *userAnswer, int *userLine, int *userColumn, int *userMove)
+void convertAnswer(char *userAnswer, int *userLine, int *userColumn, int *userMove, int *specialMove)
 {
     int answerGroup;
     int validate[4] = {0};
@@ -157,12 +210,17 @@ void convertAnswer(char *userAnswer, int *userLine, int *userColumn, int *userMo
     for (i = 1; i < 4; i++)
     {
         char letter = toupper(*(userAnswer + i));
-
-        answerGroup = classifyAnswer(letter);
+        if(*(userAnswer) == 'F' && )
+		{
+			
+		} else if(*(userAnswer) == 'S')
+		{
+			
+		}
         if (answerGroup == 0 || validate[answerGroup] == 1)
         {
             *userAnswer = 1;
-            continue;
+            break;
         }
         else
         {
